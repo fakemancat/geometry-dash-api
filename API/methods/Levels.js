e4d7f9ec9487e7004b539bf5deaabce1202a6227
fakemancat <bryuhanovdima@yandex.ru>
2018-11-23 22:05:12 +0500

    Added new method 'find'

--- API/methods/Levels.js
+++ API/methods/Levels.js
@@ -56,7 +56,7 @@ module.exports = class Levels {
             'XL'
         ][result[15]];
 
-        let password = +xor.cipher((Buffer.from(result[27], 'base64').toString()), 26364);
+        let password = +xor.decrypt(result[27], 26364);
         password = String(password);
 
         if (password.length > 1) {
@@ -101,5 +101,95 @@ module.exports = class Levels {
     }
     getWeekly() {
         return this.getById({ levelID: -2 });
+    }
+
+    async find(params = {}) {
+        // Params OMG.. And that's not all...
+        if (!params.len) params.len = '-';
+        if (!params.star) params.star = 0;
+        if (!params.page) params.page = 0;
+        if (!params.type) params.type = 0;
+        if (!params.epic) params.epic = 0;
+        if (!params.coins) params.coins = 0;
+        if (!params.query) paramError('query');
+        if (!params.feature) params.feature = 0;
+        if (!params.original) params.original = 0;
+        if (!params.twoPlayer) params.twoPlayer = 0;
+        if (!params.difficulty) params.difficulty = '-';
+
+        const len = {
+            'all': '-',
+            'tiny': 0,
+            'short': 1,
+            'medium': 2,
+            'long': 3,
+            'XL': 4
+        }[params.len] || '-';
+
+        const diff = {
+            'all': '-',
+            'NA': -1,
+            'easy': 1,
+            'normal': 2,
+            'hard': 3,
+            'harder': 4,
+            'insane': 5,
+            'demon': -2,
+            'auto': -3
+        }[params.difficulty] || '-';
+
+        const searched = await request(`${this.options.server}/getGJLevels21.php`, {
+            method: 'POST',
+            form: {
+                gameVersion: '21',
+                binaryVersion: '35',
+                gdw: '0',
+                len,
+                star: params.star,
+                page: params.page,
+                type: params.type,
+                epic: params.epic,
+                coins: params.coins,
+                str: params.query,
+                feature: params.feature,
+                original: params.original,
+                twoPlayer: params.twoPlayer,
+                diff,
+                secret: 'Wmfd2893gb7'
+            }
+        });
+
+        if (searched == '-1') {
+            return null;
+        }
+
+        const data = searched.split('#');
+        const count = +data[3].split(':')[0];
+        const levels = data[0].split('|');
+
+        const result = {
+            page: params.page,
+            pages: levels.length < 10 ? 1 : Math.ceil(count / 10),
+            count,
+            levels: []
+        };
+
+        for (let level of levels) {
+            if (level.length < 1) {
+                continue;
+            }
+
+            level = splitRes(level);
+
+            result.levels.push({
+                levelID: +level[1],
+                name: level[2],
+                creatorUserID: +level[6],
+                likes: +level[14],
+                downloads: +level[10]
+            });
+        }
+
+        return result;
     }
 };
